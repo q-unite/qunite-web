d68c33dd524134f4d220fb47c5955dd428065889
Denlich <muchalovych@gmail.com>
2023-08-12 18:49:37 +0300

    Build HideButton

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { NavbarItem } from "./NavbarItem/NavbarItem";
 import styles from "./Sidebar.module.css";
+import { Button } from "../../UI";
 
 interface Props
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
@@ -13,6 +14,14 @@ const Sidebar = ({ ...props }: Props): JSX.Element => {
         <NavbarItem icon="dashboard" name="Dashboard" href="/dashboard" />
         <NavbarItem icon="settings" name="Settings" href="/settings" />
       </ul>
+      <div className={styles.hideButtonContainer}>
+        <Button
+          className={styles.hideButton}
+          appearance="danger"
+          icon="arrow"
+          onClick={() => {}}
+        />
+      </div>
     </nav>
   );
 };
