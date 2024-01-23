5e56ed3a9a65c109d27f8ec40079f2ae23b495c4
Denlich <muchalovych@gmail.com>
2024-01-23 12:07:58 +0100

    Implement sign-in api method

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,40 +1,44 @@
-import { useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button, P } from "../../UI";
 import styles from "./LoginForm.module.css";
 import { schema, FormData } from "./schema";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import InputBox from "../InputBox";
-import { onSubmit } from "../onSubmit";
 import { useGetMe } from "../../../hooks";
+import AuthService from "../../../lib/services/auth";
+import getErrorMessage from "../../../lib/utils/getErrorMessage";
 
 const LoginForm = (): JSX.Element => {
-  const [error, setError] = useState("");
+  const navigate = useNavigate();
   const { data, refetch } = useGetMe();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
   const isAuthenticated = data?.username;
 
-  return isAuthenticated ? (
-    <Navigate to="/" />
-  ) : (
-    <form
-      className={styles.form}
-      onSubmit={handleSubmit((data: FieldValues) => {
-        onSubmit({ data, endpoint: "/auth/sign-in", setError }).then(() =>
-          refetch()
-        );
-      })}
-    >
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await AuthService.signIn(data);
+      await refetch().then(() => navigate("/"));
+    } catch (error: unknown) {
+      const message = getErrorMessage(error);
+      setError("root", { message });
+    }
+  });
+
+  if (isAuthenticated) return <Navigate to="/" />;
+
+  return (
+    <form className={styles.form} onSubmit={onSubmit}>
       <InputBox
         id="login"
         placeholder="Type your login"
@@ -49,10 +53,10 @@ const LoginForm = (): JSX.Element => {
         type="password"
         error={errors.password?.message}
       />
-      {error && <P color="primary">{error}</P>}
       <Button type="submit" appearance="success">
         Login
       </Button>
+      {errors.root && <P color="primary">{errors.root.message}</P>}
     </form>
   );
 };
--- src/lib/api/auth/AuthApi.ts
+++ src/lib/api/auth/AuthApi.ts
@@ -1,9 +1,16 @@
+import { Token } from "../../../types/token";
 import client from "../instance";
+import { SignInBody } from "./types/SignInBody";
 import { SignUpBody } from "./types/SignUpBody";
 
 class AuthApi {
   async signUp(body: SignUpBody): Promise<void> {
     const { data } = await client.post<void>("/auth/sign-up", body);
+    return data;
+  }
+
+  async signIn(body: SignInBody): Promise<Token> {
+    const { data } = await client.post<Token>("/auth/sign-in", body);
     return data;
   }
 }
